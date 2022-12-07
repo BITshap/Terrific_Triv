2c55dfb8b46d24f388e16fa6b54119163ea1b641
Nick Shapoff <nicholasshapoff@gmail.com>
2022-12-06 19:16:22 -0800

    first-frontend

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Weclome to Terrific Trivia!</h1>
+            <h3>Be Prepared for some Terrific Questions</h3>
+        </div>
+    )  
+}
+
+export default Home
