d2fea1e7e7f70a253683f0952b3b6f944108c046
Alex Schmidt <alex.schmidt767@gmail.com>
2022-12-07 21:46:34 -0600

    question schema fixed and helper method for answers added

---
+++ models/question.js
@@ -0,0 +1,31 @@
+// dependencies
+const mongoose = require('mongoose')
+
+// schema
+const questionSchema = mongoose.Schema({
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    question: { type: String, required: true },
+    correctAnswer: { type: String, required: true },
+    incorrectAnsers: [ { type: String } ]
+})
+
+// methods
+questionSchema.methods.genAnswerOrder = function(){
+    // create array of all answer options
+    let answerArray = [this.correctAnswer, ...this.incorrectAnsers]
+    
+    // shuffle answerArray
+    let currentIndex = answerArray.length
+    let randomIndex
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        [answerArray[currentIndex], answerArray[randomIndex]] = [answerArray[randomIndex], answerArray[currentIndex]]
+    }
+
+    // return array of all answer options in random order
+    return answerArray
+}
+
+module.exports = mongoose.Model('Question', questionSchema)
