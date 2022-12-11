311c29c482f0c13be56c99bd520fd5f42a838b3e
Alex Schmidt <alex.schmidt767@gmail.com>
2022-12-11 05:01:53 -0600

    updated session schema to support tracking specific answer data

--- backend/models/session.js
+++ backend/models/session.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose')
 // schema
 const sessionSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    questions: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Question' } ],
+    questions: [
+        {
+            question: { type: String },
+            correctAnswer: { type: String },
+            selectedAnswer: { type: String },
+            isCorrect: selectedAnswer === correctAnswer ? true : false
+        }
+    ],
     score: { type: Number, required: true },
     date: { type: Date }
 })
