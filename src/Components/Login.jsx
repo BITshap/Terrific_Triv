49b8609f22d7fecb60356e9c82d613e0b6e23baf
Alex Schmidt <alex.schmidt767@gmail.com>
2022-12-13 19:57:39 -0600

    user registration functioning on front and backend

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -19,9 +19,9 @@ const Login = (props) => {
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlfor="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlfor="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password" />
                 <button type="submit" className="login-button">Login</button>
             </form>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -7,14 +7,14 @@ const Register = (props) => {
     const [lastName, setLastName] = useState('');
     const [age, setAge] = useState('');
 
-    const [emailList, setEmailList] = useState([]);
+    const [userList, setUserList] = useState([]);
     const [errorMessage, setErrorMessage] = useState('')
 
     useEffect(() => {
         // fetch array of all users' emails to check for emails that have already been used
-        fetch('http://localhost:3001/users/emails')
+        fetch('http://localhost:3001/users/loginInfo')
             .then(response => response.json())
-            .then(resData => setEmailList(resData))
+            .then(resData => setUserList(resData))
             .catch(error => console.log(error.message))
     })
 
@@ -23,32 +23,35 @@ const Register = (props) => {
         
         // check email to see if account already exists
         let emailIsValid = true;
-        emailList.forEach(existingEmail => {
-            if (existingEmail === email) {
-                setErrorMessage(`${email} is already associated with a Terrific Trivia account!`)
-                emailIsValid = false;
-                // TODO: display error message and reset form
-            }
-        })
+        if (userList.length > 0) {
+            userList.forEach(user => {
+                if (user.email === email) {
+                    setErrorMessage(`${email} is already associated with a Terrific Trivia account!`)
+                    emailIsValid = false;
+                    // TODO: display error message and reset form
+                }
+            })
+        }
 
         // if email is valid POST new user
         if (emailIsValid) {
             try {
                 let res = await fetch('http://localhost:3001/users', {
                     method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
-                        firstName: firstName,
-                        lastName: lastName,
-                        email: email,
-                        password: pass,
-                        age: age,
-                        lastPlayed: new Date(0),
-                        lastLogin: new Date(),
-                        overallGrade: 'No Session Data (YET!)',
-                        sessions: []
+                        "firstName": firstName,
+                        "lastName": lastName,
+                        "email": email,
+                        "password": pass,
+                        "age": age,
+                        "lastPlayed": new Date(0),
+                        "lastLogin": new Date(),
+                        "overallGrade": 'No Session Data (YET!)',
+                        "sessions": []
                     })
                 })
-                let resJson = await res.json()
+                let resJson = await res.text()
                 if (res.status === 200) {
                     // reset field values
                     setEmail('')
@@ -69,17 +72,20 @@ const Register = (props) => {
         <div className="auth-form-container">
             <h2>Register</h2>
             <form className="register-form" onSubmit={handleSubmit}>
-                <label htmlfor="firstName">First Name:</label>
+                <label htmlFor="firstName">First Name:</label>
                 <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="firstName" placeholder="First Name" id="firstName" name="firstName" />
-                <label htmlfor="lastName">Last Name:</label>
+                <label htmlFor="lastName">Last Name:</label>
                 <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="lastName" placeholder="Last Name" id="lastName" name="lastName" />
-                <label htmlfor="age">Age:</label>
+                <label htmlFor="age">Age:</label>
                 <input value={age} onChange={(e) => setAge(e.target.value)} type="age" placeholder="Age" id="age" name="age" />
-                <label htmlfor="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlfor="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password" />
-                <button type="submit" className="login-button">Login</button>
+                <button type="submit" className="login-button">Register & Login</button>
+                {
+                    errorMessage != '' ? errorMessage : ''
+                }
             </form>
             <button className="link-btn login-button" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
         </div>
