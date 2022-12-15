66e65fba9072b37836b69e3768812c2997641b47
chusafus <106289003+chusafus@users.noreply.github.com>
2022-12-15 17:56:24 -0600

    quiz and category changes

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 
-const Category = () => {
-    return (
-       <div>
-        <p>Categories</p>
+// const Category = () => {
+//     return (
+//        <div>
+//         <p>Categories</p>
+
  const App = () => {
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
     setOpen(!open);
-  };
+  }
+}
 
-  return (
+
+{/* return (
     <div className="dropdown">
-      <button onClick={handleOpen}>Dropdown</button>
+        <button onClick={handleOpen}>Dropdown</button>
       {open ? (
         <ul className="menu">
           <li className="menu-item">
@@ -23,33 +26,40 @@ const Category = () => {
             <button>Menu 2</button>
           </li>
         </ul>
+    
       ) : null}
       {open ? <div>Is Open</div> : <div>Is Closed</div>}
+        
+        </div>
+) */}
 
+
+export default function CategorySelect(props) {
+  function setCategory(event) {
+    props.handleSetCategory(event.target.value);
+  }
+  return (
+    <Select darkMode={props.darkMode} onChange={setCategory}>
+      <option value="9">General Knowledge</option>
+      <option value="10">Entertainment: Books</option>
+      <option value="11">Entertainment: Film</option>
+      <option value="12">Entertainment: Music</option>
+      <option value="13">Entertainment: Musicals & Theatres</option>
+      <option value="14">Entertainment: Television</option>
+      <option value="15">Entertainment: Video Games</option>
+      <option value="16">Entertainment: Board Games</option>
+      <option value="17">Science and Nature</option>
+      <option value="18">Science: Computers</option>
+      <option value="19">Science: Mathematics</option>
+      <option value="20">Mythology</option>
+      <option value="21">Sports</option>
+      <option value="22">Geography</option>
+      <option value="23">History</option>
+      <option value="24">Politics</option>
+      <option value="25">Art</option>
+      <option value="26">Celebrities</option>
+      <option value="27">Animals</option>
+      <option value="28">Vehicles</option>
+    </Select>
+  )
 }
-
-    </div>
-    export default function CategorySelect(props) {
-        function setCategory(event) {
-          props.handleSetCategory(event.target.value);
-        }
-        return (
-          <Select darkMode={props.darkMode} onChange={setCategory}>
-            <option value="9">General Knowledge</option>
-            <option value="10">Entertainment: Books</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="12">Entertainment: Music</option>
-            <option value="13">Entertainment: Musicals & Theatres</option>
-            <option value="14">Entertainment: Television</option>
-            <option value="15">Entertainment: Video Games</option>
-            <option value="16">Entertainment: Board Games</option>
-            <option value="17">Science and Nature</option>
-            <option value="18">Science: Computers</option>
-            <option value="19">Science: Mathematics</option>
-            <option value="20">Mythology</option>
-
-          </Select>
-        );
-      }
-  );
-};
